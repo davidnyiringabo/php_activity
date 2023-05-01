1bc1aa978cd6063c66e28194bfe8ab8c6dc4d3e3
David_NYIRI <nyiringabodavid62@gmail.com>
2023-05-01 23:13:33 +0200

    new features added

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>home page</title>
+</head>
+<body>
+    <h1 class="text-center bg-primary-subtle p-2">students</h1>
+    <div class="container">
